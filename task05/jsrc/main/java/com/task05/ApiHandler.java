73c28a3bfe1d372ea11ace366ed30ef401a9e2c1
Dmytro Kyslukha <dmytro_kyslukha@epam.com>
2024-08-08 19:48:17 +0300

    task05-6

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -5,37 +5,51 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.events.DynamoDbTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 import org.joda.time.format.ISODateTimeFormat;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 @LambdaHandler(lambdaName = "api_handler", roleName = "api_handler-role", isPublishVersion = false, logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
 @DynamoDbTriggerEventSource(targetTable = "Events", batchSize = 10)
+@EnvironmentVariable(key = "TABLE_NAME", value = "${target_table}")
 public class ApiHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+    public ApiHandler(){
+//        tableName = "cmtr-3ba132da-Events";
+        tableName = System.getenv("TABLE_NAME");
+        table = dynamoDB.getTable(tableName);
+    }
 
-    private static final String DYNAMODB_TABLE_NAME = "Events" ;
+    private  final String tableName;
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
     private final com.amazonaws.services.dynamodbv2.document.DynamoDB dynamoDB = new com.amazonaws.services.dynamodbv2.document.DynamoDB(client);
-    private final Table table = dynamoDB.getTable(DYNAMODB_TABLE_NAME);
+    private final Table table;
 
     @Override
     public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
         try {
+            LambdaLogger logger = context.getLogger();
+            logger.log("tableName"+tableName);
+            logger.log("request"+request.toString());
             Map<String, Object> responseBody = new HashMap<>();
             String id = UUID.randomUUID().toString();
             Integer principalId = (Integer) request.get("principalId");
-            String createdAt = ISODateTimeFormat.dateTimeNoMillis().toString();
-            Map<String, String> content = (Map<String, String>) request.get("content");
-            Map<String,Map<String,String>> body = new HashMap<>();
-            body.put("content", content);
+            String createdAt = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+            Map<String, String> body = (Map<String, String>) request.get("content");
+//            Map<String,Map<String,String>> body = new HashMap<>();
+//            body.put("content", content);
             Item item = new Item().withPrimaryKey("id", id).withInt("principalId", principalId)
                     .withString("createdAt", createdAt).withMap("body", body);
             table.putItem(item);

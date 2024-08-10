7b19bda8315386ce4b9f4a911ce61787276cadc6
Dmytro Kyslukha <dmytro_kyslukha@epam.com>
2024-08-11 02:32:59 +0300

    task07-6

--- task07/jsrc/main/java/com/task07/UuidGenerator.java
+++ task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -5,23 +5,22 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.events.RuleEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.resources.DependsOn;
-import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+
+import static com.amazonaws.util.json.Jackson.getObjectMapper;
 
 @LambdaHandler(
         lambdaName = "uuid_generator",
@@ -34,6 +33,7 @@ import java.util.UUID;
 @EnvironmentVariables(value = {
         @EnvironmentVariable(key = "region", value = "eu-central-1"),
         @EnvironmentVariable(key = "notification_bucket", value = "uuid-storage")
+//        @EnvironmentVariable(key = "notification_bucket", value = "cmtr-3ba132da-uuid-storage")
 })
 @RuleEventSource(
         targetRule = "uuid_trigger"
@@ -41,7 +41,7 @@ import java.util.UUID;
 
 public class UuidGenerator implements RequestHandler<Object, String> {
 
-    	private static final String BUCKET_NAME = "uuid-storage";
+        	private static final String BUCKET_NAME = "uuid-storage";
 //    private static final String BUCKET_NAME = "cmtr-3ba132da-uuid-storage";
 
 
@@ -54,24 +54,23 @@ public class UuidGenerator implements RequestHandler<Object, String> {
             uuidList.add(UUID.randomUUID().toString());
         }
 
-        String uuidData = String.join("\n", uuidList);
-        logger.log("uuid list " + uuidData);
+        Map<String, List<String>> fileUUID = new HashMap<>();
+        fileUUID.put("ids", uuidList);
+        logger.log("fileUUID " + fileUUID.toString());
 
         String timestamp = DateTimeFormatter.ISO_INSTANT.format(Instant.now());
-        String fileName = "uuids_" + timestamp + ".txt";
+        String fileName = "uuids_" + timestamp + ".json";
         logger.log("filename " + fileName);
 
-        uploadToS3(uuidData, fileName);
+        try {
+            String fileContentJson = getObjectMapper().writeValueAsString(fileUUID);
+            AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+            s3Client.putObject(BUCKET_NAME, fileName, fileContentJson);
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
 
         return "File uploaded: " + fileName;
-    }
-
-    private void uploadToS3(String uuidData, String fileName) {
-        AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
-        InputStream inputStream = new ByteArrayInputStream(uuidData.getBytes(StandardCharsets.UTF_8));
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(uuidData.length());
-
-        s3Client.putObject(BUCKET_NAME, fileName, inputStream, metadata);
     }
 }

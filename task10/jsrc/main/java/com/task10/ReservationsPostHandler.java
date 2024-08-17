b55577a552463734856ab61140a8d2dcdb9f2acf
Dmytro Kyslukha <dmytro_kyslukha@epam.com>
2024-08-17 14:18:52 +0300

    task10-1

--- task10/jsrc/main/java/com/task10/ReservationsPostHandler.java
+++ task10/jsrc/main/java/com/task10/ReservationsPostHandler.java
@@ -3,74 +3,63 @@ package com.task10;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.RangeKeyCondition;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class ReservationsPostHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private final DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
-
-//    private final String RESERVATIONS = System.getenv("NAME_TABLE_RESERVATIONS");
-    private final String RESERVATIONS = "cmtr-3ba132da-Reservations-test";
+    private final String RESERVATIONS = System.getenv("NAME_TABLE_RESERVATIONS");
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
         try {
             Map bodyMap = new ObjectMapper().readValue(request.getBody(), Map.class);
 
-            Object tableNumber = bodyMap.get("tableNumber");
-            Object clientName = bodyMap.get("clientName");
-            Object phoneNumber = bodyMap.get("phoneNumber");
-            Object date = bodyMap.get("date");
-            Object slotTimeStart = bodyMap.get("slotTimeStart");
-            Object slotTimeEnd = bodyMap.get("slotTimeEnd");
+            Integer tableNumber = (Integer) bodyMap.get("tableNumber");
+            String clientName = (String) bodyMap.get("clientName");
+            String phoneNumber = (String) bodyMap.get("phoneNumber");
+            String date = (String) bodyMap.get("date");
+            String slotTimeStart = (String) bodyMap.get("slotTimeStart");
+            String slotTimeEnd = (String) bodyMap.get("slotTimeEnd");
 
-
-            Integer tableNumberInt = (Integer) tableNumber;
-            String clientNameStr = (String) clientName;
-            String phoneNumberStr = (String) phoneNumber;
-            String dateStr = (String) date;
-            String slotTimeStartStr = (String) slotTimeStart;
-            String slotTimeEndStr = (String) slotTimeEnd;
-
-            if (clientNameStr.isEmpty() || phoneNumberStr.isEmpty() || dateStr.isEmpty() ||
-                    slotTimeStartStr.isEmpty() || slotTimeEndStr.isEmpty()) {
+            if (clientName == null || clientName.isEmpty() ||
+                    phoneNumber == null || phoneNumber.isEmpty() ||
+                    date == null || date.isEmpty() ||
+                    slotTimeStart == null || slotTimeStart.isEmpty() ||
+                    slotTimeEnd == null || slotTimeEnd.isEmpty()) {
                 return new APIGatewayProxyResponseEvent()
                         .withStatusCode(400)
                         .withBody("There was an error in the request.");
             }
-//            if (!tableExists(tableNumberInt)) {
-//                return new APIGatewayProxyResponseEvent()
-//                        .withStatusCode(400)
-//                        .withBody("There was an error in the request.");
-//            }
-//            if (conflictingReservationExists(tableNumberInt, dateStr, slotTimeStartStr, slotTimeEndStr)) {
-//                return new APIGatewayProxyResponseEvent()
-//                        .withStatusCode(400)
-//                        .withBody("There was an error in the request.");
-//            }
+
+            if (conflictingReservationExists(tableNumber, date, slotTimeStart, slotTimeEnd)) {
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(400)
+                        .withBody("There was an error in the request.");
+            }
 
             String reservationId = UUID.randomUUID().toString();
-
             Item item = new Item().withPrimaryKey("id", reservationId)
-                    .withInt("tableNumber", tableNumberInt)
-                    .withString("clientName", clientNameStr)
-                    .withString("phoneNumber", phoneNumberStr)
-                    .withString("date", dateStr)
-                    .withString("slotTimeStart", slotTimeStartStr)
-                    .withString("slotTimeEnd", slotTimeEndStr);
-
+                    .withInt("tableNumber", tableNumber)
+                    .withString("clientName", clientName)
+                    .withString("phoneNumber", phoneNumber)
+                    .withString("date", date)
+                    .withString("slotTimeStart", slotTimeStart)
+                    .withString("slotTimeEnd", slotTimeEnd);
 
             Table table = dynamoDB.getTable(RESERVATIONS);
-
             table.putItem(item);
 
             return new APIGatewayProxyResponseEvent()
@@ -88,29 +77,23 @@ public class ReservationsPostHandler implements RequestHandler<APIGatewayProxyRe
                                                  String slotTimeStart, String slotTimeEnd) {
         Table reservationsTable = dynamoDB.getTable(RESERVATIONS);
 
-        Map<String, String> expressionAttributeNames = new HashMap<>();
-        expressionAttributeNames.put("#tableNumber", "tableNumber");
-        expressionAttributeNames.put("#date", "date");
-        expressionAttributeNames.put("#slotTimeStart", "slotTimeStart");
-        expressionAttributeNames.put("#slotTimeEnd", "slotTimeEnd");
+        QuerySpec spec = new QuerySpec()
+                .withKeyConditionExpression("#tableNumber = :tableNumber and #date = :date")
+                .withFilterExpression("#slotTimeStart < :slotTimeEnd AND #slotTimeEnd > :slotTimeStart")
+                .withNameMap(new NameMap()
+                        .with("#tableNumber", "tableNumber")
+                        .with("#date", "date")
+                        .with("#slotTimeStart", "slotTimeStart")
+                        .with("#slotTimeEnd", "slotTimeEnd"))
+                .withValueMap(new ValueMap()
+                        .withInt(":tableNumber", tableNumber)
+                        .withString(":date", date)
+                        .withString(":slotTimeStart", slotTimeStart)
+                        .withString(":slotTimeEnd", slotTimeEnd));
 
-        Map<String, Object> expressionAttributeValues = new HashMap<>();
-        expressionAttributeValues.put(":tableNumber", tableNumber);
-        expressionAttributeValues.put(":date", date);
-        expressionAttributeValues.put(":slotTimeStart", slotTimeStart);
-        expressionAttributeValues.put(":slotTimeEnd", slotTimeEnd);
+        ItemCollection<QueryOutcome> items = reservationsTable.query(spec);
 
-        return reservationsTable.query(
-                "#tableNumber = :tableNumber AND #date = :date AND (#slotTimeStart < :slotTimeEnd AND #slotTimeEnd > :slotTimeStart)",
-                (Object) expressionAttributeNames,
-                (RangeKeyCondition) expressionAttributeValues
-        ).iterator().hasNext();
-    }
-
-    private boolean tableExists(Integer tableNumberInt) {
-        Table table = dynamoDB.getTable(RESERVATIONS);
-        Item tableItem = table.getItem("tableNumber", tableNumberInt);
-        return tableItem != null;
+        return items.iterator().hasNext();
     }
 
 }
